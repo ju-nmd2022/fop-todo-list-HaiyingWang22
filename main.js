a6c1b6e3ba0464bd6eac90363a24d643e0e4f37e
HaiyingWang22 <horizont072@gmail.com>
2023-04-05 22:36:30 +0200

    add value to array

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 const btn = document.getElementById("input-btn")
+let tip = document.getElementById("tip")
 let Value_list = [];
 
 function Get_value(){
     let inpute_Value = document.getElementById("Input").value;
     if (inpute_Value !==""){
-        alert("文本为: " + inpute_Value);
+        Value_list.push(String(inpute_Value));
+        document.getElementById("Input").value = "";
+        tip.style.display="none";
     }
-};
+}
+console.log(Value_list);
+
+// if(Value_list.length === 0){
+// }
+
+
+
+
+
 btn.addEventListener("click", Get_value);
