dd9064d49ff982c32df8a58f00cec3a4682c9f71
HaiyingWang22 <horizont072@gmail.com>
2023-04-06 16:19:12 +0200

    add  function

--- main.js
+++ main.js
@@ -1,22 +1,37 @@
-const btn = document.getElementById("input-btn")
+const input_btn = document.getElementById("input-btn")
+const item = document.getElementById("item");
 let tip = document.getElementById("tip")
 let Value_list = [];
 
+input_btn.addEventListener("click", Get_value);
+
 function Get_value(){
     let inpute_Value = document.getElementById("Input").value;
-    if (inpute_Value !==""){
+    if (inpute_Value !==""){  // if input is not empty
         Value_list.push(String(inpute_Value));
         document.getElementById("Input").value = "";
         tip.style.display="none";
+        creat_element (inpute_Value);
+        // Value_list.forEach((value) =>{
+        //     console.log(value);
+        // });
     }
 }
-console.log(Value_list);
-
-// if(Value_list.length === 0){
-// }
 
 
+function creat_element (inpute_Value) {
+    const content_element= document.createElement("div");
+    content_element.classList.add("content");
+    content_element.innerText = inpute_Value;
+    item.appendChild(content_element);
+
+
+    const chick = document.createElement("button")
+    chick.classList.add("content");
+
+
+}
 
 
 
-btn.addEventListener("click", Get_value);
+
