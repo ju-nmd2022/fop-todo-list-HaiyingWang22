d9e040f13af11f6ad0b1c1a62fdb5540fc485d37
HaiyingWang22 <horizont072@gmail.com>
2023-04-06 17:51:35 +0200

    add button

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const input_btn = document.getElementById("input-btn")
-const item = document.getElementById("item");
+const Todo_list = document.getElementById("to-do-list");
 let tip = document.getElementById("tip")
 let Value_list = [];
 
@@ -11,25 +11,44 @@ function Get_value(){
         Value_list.push(String(inpute_Value));
         document.getElementById("Input").value = "";
         tip.style.display="none";
-        creat_element (inpute_Value);
-        // Value_list.forEach((value) =>{
-        //     console.log(value);
-        // });
+        Todo_list.innerHTML = "";  //refresh page
+        Value_list.forEach((value) =>{
+            creat_element (value);
+        });
     }
 }
 
 
-function creat_element (inpute_Value) {
+function creat_element (value) {
+    const item = document.createElement("div");
+    item.classList.add("item");
+
+// content
     const content_element= document.createElement("div");
     content_element.classList.add("content");
-    content_element.innerText = inpute_Value;
+    content_element.innerText = value;
     item.appendChild(content_element);
 
+// chick button
+    const chick = document.createElement("button");
+    chick.classList.add("chick-btn");
+    const icon1 = document.createElement("span");
+    icon1.classList.add("material-symbols-outlined");
+    icon1.innerText = ("check_box");
+    chick.appendChild(icon1);
+    item.appendChild(chick);
 
-    const chick = document.createElement("button")
-    chick.classList.add("content");
+// delete button
+    const Delete_btn = document.createElement("button");
+    Delete_btn.classList.add("delete-btn");
+    const icon3 = document.createElement("span");
+    icon3.classList.add("material-symbols-outlined");
+    icon3.innerText = ("delete");
+    Delete_btn.appendChild(icon3);
+    item.appendChild(Delete_btn);
 
 
+    Todo_list.appendChild(item);
 }
 
 
